fd3f1b4a841e04bb6d369c734f50c10cd81b8db0
smriti1990 <smriti.jaiswal24@gmail.com>
2024-09-02 07:11:00 +0000

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+
+module "website_s3_bucket" {
+  source = "./modules/aws-s3-static-website-bucket"
+
+  bucket_name = "smriti-bucket-02092024"
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
